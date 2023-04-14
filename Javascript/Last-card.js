da4994714a2aec6582878f6a7545e576db07d5e9
Jefry <jefryconde@gmail.com>
2023-04-13 21:13:11 -0500

    I have called up each of the keys I had stored and rendered them in the last-card paragraphs

---
+++ Javascript/Last-card.js
@@ -0,0 +1,9 @@
+const result1 = document.getElementById('resultOne').innerText = sessionStorage.getItem('question1');
+const result2 = document.getElementById('resultTwo').innerText = sessionStorage.getItem('question2');
+const result3 = document.getElementById('resultThree').innerText = sessionStorage.getItem('question3');
+
+if ('result1' === tre) {
+    addClass(correctAnswer)
+} else {
+    addClass(incorrectAnswer)
+}
